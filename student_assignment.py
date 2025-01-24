b9404198aff6f3c8a332329b544bcd79753ba6c5
renaht_wang <RenaHT_Wang@asus.com>
2025-01-24 17:28:17 +0800

    first try

--- student_assignment.py
+++ student_assignment.py
@@ -1,13 +1,36 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import (CharacterTextSplitter,
                                       RecursiveCharacterTextSplitter)
+import re
 
+    
 q1_pdf = "OpenSourceLicenses.pdf"
 q2_pdf = "勞動基準法.pdf"
 
 
 def hw02_1(q1_pdf):
-    pass
+    loader = PyPDFLoader(q1_pdf)
+    pdf = loader.load()
+    c_splitter = CharacterTextSplitter(chunk_overlap= 0)
+    chunk = c_splitter.split_documents(pdf)
+    return chunk[len(chunk) - 1]
+    
 
 def hw02_2(q2_pdf):
-    pass
+    loader = PyPDFLoader(q2_pdf)
+    pdf = loader.load()
+    full_text = "\n".join([page.page_content for page in pdf])
+    r_splitter = RecursiveCharacterTextSplitter(
+        separators=[r'第 +[一二三四五六七八九十0-9\-]+ +[章條]'],
+        chunk_size=10,
+        chunk_overlap=0,
+        is_separator_regex=True,
+    )
+    chunk = r_splitter.split_text(full_text)
+
+    return len(chunk)
+
+    
+
+# print(hw02_1(q1_pdf))
+# print(hw02_2(q2_pdf))
